d81483975b1fb8e514dbc5bb35ebe3cacb474ac9
Aakarsh Yelisetty <aakarshyelisetty@Aakarshs-MacBook-Air.local>
2025-04-16 19:44:11 +0530

    Update axios URL

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://sf-dashboard-backend.onrender.com',
 })
 
 // ✅ Interceptor grabs token from localStorage on every request
